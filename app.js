a0b15b14ff45805f7300e22e44ba0d0f249551d7
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-26 15:08:43 -0400

    ITS DONE Finally we can add remove books in our library

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const submitBtn = document.querySelector("#submitbtn");
 const isRead = document.querySelector("#read");
 
 const cardContainer = document.querySelector(".card-container")
+const card = document.querySelector(".card")
 
 const divForm = document.querySelector(".form");
+const element = ''
 
 let myLibrary = []
 
@@ -26,11 +28,33 @@ function addBookToLibrary() {
         e.preventDefault()
         var book = new Book(bookName.value , author.value , pages.value , isRead.value);
         myLibrary.push(book);
-        for(let i = 0 ; i < myLibrary.length ; i++) {
-            cardContainer.innerHTML += `<div class="card"><h1>${myLibrary[i].name}</h1></card>`
-        }
+        myLibrary.forEach(textBook => { 
+            cardContainer.innerHTML += `<div class="card" data-index = ${myLibrary.indexOf(textBook)}><h3>${textBook.name}</h3><h3>${textBook.author}</h3><h3>${textBook.pages}</h3><button class="remove" data-index = ${myLibrary.indexOf(textBook)}>Remove Book</button></card>`
+            let removebtns = document.querySelectorAll('.remove');
+            const removebtnsarr = Array.from(removebtns)
+            removeBook(removebtnsarr);
+        }) 
+        
+        
     })
+    
+    
 }
 
 addBookToLibrary();
+
+function removeBook(nl) {
+    nl.forEach(removebtn => {
+        removebtn.addEventListener('click' , function() {
+            let cardIndex = removebtn.getAttribute('data-index')
+            let index = nl.indexOf(removebtn)
+            nl.splice(index , 1);
+            myLibrary.splice(index, 1);
+            let removeCard = document.querySelector(`[data-index="${cardIndex}"]`)
+            cardContainer.removeChild(removeCard);
+            
+        })
+    })
+
+}
 
