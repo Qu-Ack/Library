a48da79384787e188b39fc76ee6702e09077d8d3
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-27 09:22:04 -0400

    we have almost completed the project but theres one small bug that is when we add a new book all the read value reset which we will have to fix and then we aree done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const addBtn = document.querySelector('.Add-Book');
+
 const bookName = document.querySelector('#bookname');
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
@@ -6,12 +7,29 @@ const submitBtn = document.querySelector("#submitbtn");
 const isRead = document.querySelector("#read");
 
 const cardContainer = document.querySelector(".card-container")
+const formClass = document.querySelector('.form-reset')
 const card = document.querySelector(".card")
 
 const divForm = document.querySelector(".form");
 const element = ''
 
+
+
 let myLibrary = []
+
+
+addBtn.addEventListener('click' , function() {
+    divForm.style.opacity = 1;
+    divForm.style.transform = 'scale(1)';
+    divForm.classList.add('popup')
+    document.addEventListener('keydown' , function(e) {
+        if (e.code == "Escape") {
+        divForm.classList.remove('popup')
+    }
+    })
+     
+})
+
 
 
 
@@ -20,26 +38,53 @@ function Book(name , author , pages, read) {
     this.author = author
     this.pages = pages
     this.read = read;
+    this.readBook = function(readbtn) {
+        if (read) {
+            readbtn.textContent = "Not Read"
+            this.read = false;
+        }
+        else {
+            readbtn.textContent = "Read"
+            this.read = true;
+        }
+    } 
 }
+
+
+
+
 
 function addBookToLibrary() {
     submitBtn.addEventListener("click" , function(e) {
         cardContainer.innerHTML = '';
         e.preventDefault()
-        var book = new Book(bookName.value , author.value , pages.value , isRead.value);
+        let initialReadBtnValue = '';
+        if (isRead.checked) {
+            initialReadBtnValue = 'not read';
+        }
+        else {
+            initialReadBtnValue = 'read'
+        }
+        var book = new Book(bookName.value , author.value , pages.value , initialReadBtnValue);
         myLibrary.push(book);
         myLibrary.forEach(textBook => { 
-            cardContainer.innerHTML += `<div class="card" data-index = ${myLibrary.indexOf(textBook)}><h3>${textBook.name}</h3><h3>${textBook.author}</h3><h3>${textBook.pages}</h3><button class="remove" data-index = ${myLibrary.indexOf(textBook)}>Remove Book</button></card>`
+            cardContainer.innerHTML += `<div class="card" data-index = ${myLibrary.indexOf(textBook)}><h3>${textBook.name}</h3><h3>${textBook.author}</h3><h3>${textBook.pages}</h3><button class="remove" data-index = ${myLibrary.indexOf(textBook)}>Remove Book</button><button class="readbtn">${textBook.read}</button></card>`
             let removebtns = document.querySelectorAll('.remove');
+            let readbtns = document.querySelectorAll(".readbtn")
             const removebtnsarr = Array.from(removebtns)
             removeBook(removebtnsarr);
+            bookRead(readbtns , book)
         }) 
+        formClass.reset();
+        divForm.style.opacity = 0;
+        divForm.style.transform = 'scale(0.001)'
+        
         
         
     })
     
     
-}
+} 
 
 addBookToLibrary();
 
@@ -56,5 +101,24 @@ function removeBook(nl) {
         })
     })
 
+}
+
+function bookRead(readbtns , item) {
+    readbtns.forEach(element => {
+        element.addEventListener('click' , function() {
+            if (element.textContent == 'read') {
+                element.textContent = 'not read';
+                item.read = 'read'
+                element.style.background = "red";
+            }
+            else if (element.textContent == 'not read') {
+                element.textContent = 'read'
+                item.read = 'not read'
+                element.style.background = 'green';
+            }
+            
+        })
+    });
+    
 }
 
